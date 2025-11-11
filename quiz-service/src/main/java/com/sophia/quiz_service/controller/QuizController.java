6542cb9cdb0d9f8e38d8292c8bd290285b4de5ca
SOPHIA MARY R <sophiamary.r@gmail.com>
2025-11-11 13:37:17 +0530

    Added Environment to track the service instance that is actually hit to track laod balancer

--- quiz-service/src/main/java/com/sophia/quiz_service/controller/QuizController.java
+++ quiz-service/src/main/java/com/sophia/quiz_service/controller/QuizController.java
@@ -16,11 +16,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.core.env.Environment;
 
 
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
+
+    @Autowired
+    Environment env;
 
     private QuizService service;
     @Autowired
@@ -30,6 +34,7 @@ public class QuizController {
 
     @PostMapping("/create")
     public ResponseEntity<List<Integer>> createQuiz(@RequestBody QuizDataTranferObject quizDTO) {
+        System.out.println("Quiz service running on port: " + env.getProperty("local.server.port"));
         return service.createQuiz(quizDTO.getCategory(), quizDTO.getNumQuestions(), quizDTO.getTitle());
     }
     
